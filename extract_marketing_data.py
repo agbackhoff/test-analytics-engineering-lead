c1b5caaaba8d328d8456ebc8019c51de6aa09645
Adrian Dario Garcia Backhoff <adrian@dataexpress.tech>
2024-10-13 14:56:43 -0600

    Created python script to download the datasets as csv's and load them into dbt seeds. Deleted example models to prepare workspace for development. 	deleted:    analytics_test/models/example/my_first_dbt_model.sql 	deleted:    analytics_test/models/example/my_second_dbt_model.sql 	modified:   analytics_test/models/example/schema.yml 	deleted:    analytics_test/seeds/.gitkeep 	new file:   analytics_test/seeds/features.csv 	new file:   analytics_test/seeds/targets.csv 	new file:   extract_marketing_data.py 	modified:   requirements.txt

---
+++ extract_marketing_data.py
@@ -0,0 +1,12 @@
+from ucimlrepo import fetch_ucirepo
+# fetch dataset
+bank_marketing = fetch_ucirepo(id=222)
+
+X = bank_marketing.data.features
+y = bank_marketing.data.targets
+
+X.to_csv("analytics_test/seeds/features.csv", index=False)
+y.to_csv("analytics_test/seeds/targets.csv", index=False)
+
+print("CSV files exported to analytics_test/seeds/")
+
